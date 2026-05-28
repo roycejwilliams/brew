34924d045e33c2398bfff60e8d21492817cb3433
Royce Williams <roycewilliamsj@gmail.com>
2026-05-28 16:13:57 -0700

    updated layout test

--- frontend/src/app/(app)/pulse/page.tsx
+++ frontend/src/app/(app)/pulse/page.tsx
@@ -1,9 +1,16 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Map from "../../components/BrewMap";
 
 export const dynamic = "force-dynamic";
 
 export default function Discover() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "transparent";
+    return () => {
+      document.body.style.backgroundColor = "#0c0c0c";
+    };
+  }, []);
+
   return <Map />;
 }
